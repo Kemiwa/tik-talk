41390f9dd1bb4a1d9e3cc0d72b92b3d7ab23cf72
Dmall <MTkach@uczen.eduwarszawa.pl>
2024-11-03 01:01:36 +0100

    update app.component.ts
    Finished Component and made base API

--- src/app/common-ui/profile-card/profile-card.component.ts
+++ src/app/common-ui/profile-card/profile-card.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-profile-card',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
 
 
   templateUrl: './profile-card.component.html',
